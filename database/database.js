014f0a941a3d07a2f5dc14dc21c1c358a2269fad
Amel Mohammedi <amel.m20@gmail.com>
2025-06-01 17:45:02 +0200

    Env PG url modified for Railway

--- database/database.js
+++ database/database.js
@@ -1,7 +1,15 @@
-import pg from 'pg';
+import { Pool } from 'pg';
 
-const client = new pg.Client(process.env.PG_URL);
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
 
-await client.connect();
+// Important : définir le schéma par défaut
+pool.on('connect', (client) => {
+  client.query('SET search_path TO ocoffee, public');
+});
 
-export default client;
+export { pool };
