768040713b6c94341a8cd4348b9c9e7cd2f10575
Amel Mohammedi <amel.m20@gmail.com>
2025-06-02 10:27:59 +0200

    Database file modified for railway

--- database/database.js
+++ database/database.js
@@ -3,7 +3,9 @@ const { Pool } = pkg;
 
 const client = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 export default client;
